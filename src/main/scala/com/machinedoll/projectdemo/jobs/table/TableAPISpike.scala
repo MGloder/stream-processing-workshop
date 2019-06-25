30bafda0884f9336b9dac7f0d09a064bc1152fef
eric_yan <xyan@thoughtworks.com>
2019-06-25 10:52:29 +0800

    added table pom

---
+++ src/main/scala/com/machinedoll/projectdemo/jobs/table/TableAPISpike.scala
@@ -0,0 +1,41 @@
+package com.machinedoll.projectdemo.jobs.table
+
+import java.util.Properties
+
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.table.api.TableSchema
+import org.apache.flink.table.api.scala.StreamTableEnvironment
+import org.apache.flink.table.descriptors.{Csv, Kafka, Schema}
+
+object TableAPISpike {
+
+
+  def main(args: Array[String]): Unit = {
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+
+//    val typeInfo = {Types.STRING}
+//    val tableSchema: TableSchema = TableSchema.builder().field("aa", Types.STRING).build()
+    val properties = new Properties()
+    properties.setProperty("bootstrap.servers", "localhost:9092")
+    // only required for Kafka 0.8
+    properties.setProperty("zookeeper.connect", "localhost:2181")
+    properties.setProperty("group.id", "test")
+
+    val topic = "gdelt-topic-example"
+//    var schema: TableSchema = new TableSchema()
+
+    val tableEnv = StreamTableEnvironment.create(env)
+//    tableEnv.connect(new Kafka().version("2.2.0")
+//        .topic(topic)
+//        .property("bootstrap.servers", "localhost:9092")
+//        .property("zookeeper.connect", "localhost:2181")
+//        .property("group.id", "test")
+//        .startFromLatest()
+//    ).withFormat()
+//      .withSchema(new Schema()
+//      .schema(schema)
+//        .proctime()
+//    )
+//    val tableSource = new KafkaTableSource("gdelt-topic-example", properties, SimpleStringSchema, typeInfo)
+  }
+}
